1157e78d6098276e85fd69ff7b354dfc31a818bf
Juliet Brown <juliet@silverstripe.com>
2017-07-11 19:48:49 +1200

    updated icon  animation in footer

---
+++ src/AnimieSvg.jsx
@@ -0,0 +1,15 @@
+// var React = require('react');
+// var Anime = require('animiejs');
+
+// var AnimeComponent = React.createClass({
+
+//     render: function() {
+//         return (
+//             <div className="">
+//                 <i className="icon icon--juliet-logo" />
+//             </div>
+//         );
+//     },
+// });
+
+// module.exports = AnimeComponent;
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var Anime = require('anime');
 var AnimateFooter = require('./AnimateFooter');
 
 var Footer = React.createClass({
@@ -9,6 +10,12 @@ var Footer = React.createClass({
                 <div className="footer__container section-container">
                      <div className="footer__logo"></div>
                     <AnimateFooter />  
+
+                    <div id="lineDrawing" className="icon icon--juliet-logo" ref={function(section) {
+                        self.section = section;
+                    }}>
+                    </div>
+
                     <i className="icon icon--footer-logo" />
                     <div className="icon icon--footer-email"></div>
                     <div className="icon icon--facebook"></div>
@@ -18,6 +25,25 @@ var Footer = React.createClass({
             </footer>
         );
     },
+
+    componentDidMount: function() {
+        this.animate();
+    },
+
+    animate: function() {
+
+
+    var lineDrawing = anime({
+        targets: '#lineDrawing .lines path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function(el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: true
+    });
+     
+    }
 });
 
 module.exports = Footer;
